a7539f2f0ec60f3379202882e41d4290beecb294
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-11-29 15:37:31 -0800

    Update main.c

--- main.c
+++ main.c
@@ -21,8 +21,15 @@
 #include "nokia5110.h"
 #endif
 
-enum States {Start, Init, Game, Release};
+void ADC_init() {
+    ADMUX = (1 << REFS0);
+    
+    // ADC Enable and prescalar 128
+    ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
 
+enum States {Start, Init, Game, Release, Score, Win};
+unsigned char scores = 17;
 
 int Tick(int state){
 switch(state){
@@ -30,28 +37,84 @@ switch(state){
                 state = Init;
                 break;
         case Init:
-                if((~PINA & 0x03) == 0x01){
+                if((~PINA & 0xC0) == 0x40){
+                        scores = scores + 1;
                         state = Game;
-                }else if((~PINA & 0x03) == 0x02){
+                }else if((~PINA & 0xC0) == 0x80){
                         state = Release;
+                }else if((~PINA & 0xC0) == 0xC0){
+                        state = Score;
                 }else{
                         state = Init;
                 }
                 break;
         case Game:
-                LCD_DisplayString(1, "Game Start            ");
-                if((~PINA & 0x03) == 0x02){
+                LCD_DisplayString(1, "Let's Play            ");
+                if((~PINA & 0xC0) == 0x80){
                         state = Release;
+                }else if((~PINA & 0xC0) == 0xC0){
+                        state = Score;
                 }else{
                         state = Game;
                 }
                 break;
         case Release:
                 LCD_DisplayString(1, "Game Reset?           ");
-                if((~PINA & 0x03) == 0x02){
+                if((~PINA & 0xC0) == 0x80){
+                        scores = scores + 1;
                         state = Game;
                 }else{
                         state = Release;
+                }
+                break;
+        case Score:
+                LCD_DisplayString(1, "Score: ");
+
+                if (scores < 10) {
+                LCD_Cursor(8);
+                LCD_WriteData(scores + '0');
+                LCD_Cursor(9);
+                LCD_WriteData(' ');
+                LCD_Cursor(10);
+                LCD_WriteData(' ');
+                LCD_Cursor(11);
+                LCD_WriteData(' ');
+                LCD_Cursor(12);
+                LCD_WriteData(' ');
+                } else if (scores < 25) {
+                unsigned char a = scores / 10;
+                unsigned char b = scores % 10;
+                LCD_Cursor(8);
+                LCD_WriteData(a + '0');
+                LCD_Cursor(9);
+                LCD_WriteData(b + '0');
+                LCD_Cursor(10);
+                LCD_WriteData(' ');
+                LCD_Cursor(11);
+                LCD_WriteData(' ');
+                LCD_Cursor(12);
+                LCD_WriteData(' ');
+                }
+
+                if((~PINA & 0xC0) == 0x40){
+                        scores = 0;
+                        state = Game;
+                }else if((~PINA & 0xC0) == 0x80){
+                        state = Release;
+                }else{
+                        state = Score;
+                }
+                break;
+        case Win:
+                LCD_DisplayString(1, "You win, click anything to reset");
+                if((~PINA & 0xC0) == 0x40){
+                        state = Init;
+                }else if((~PINA & 0xC0) == 0x80){
+                        state = Init;
+                }else if((~PINA & 0xC0) == 0xC0){
+                        state = Init;
+                }else{
+                        state = Win;
                 }
                 break;
         default:
@@ -68,12 +131,17 @@ switch(state){
                 break;
         case Release:
                 break;
+        case Score:
+                break;
+        case Win:
+                break;
         default:
                 break;
 }
 
 return state;
 }
+
 
 int main(void){
     DDRA = 0x00; PORTA = 0xFF;
