f2ada6b51c010a2168f3d2c567d47610b865305d
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-11-29 15:37:59 -0800

    Update main.c

--- main.c
+++ main.c
@@ -23,9 +23,16 @@
 
 void ADC_init() {
     ADMUX = (1 << REFS0);
-    
-    // ADC Enable and prescalar 128
     ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+// http://maxembedded.com/2011/06/the-adc-of-the-avr/
+unsigned short readADC(uint8_t ch) {
+    ch &= 0x07;
+    ADMUX = (ADMUX & 0xF8) | ch;
+    ADCSRA |= (1 << ADSC);
+    while ( (ADCSRA) & (1 << ADSC) );
+    return ADC;
 }
 
 enum States {Start, Init, Game, Release, Score, Win};
